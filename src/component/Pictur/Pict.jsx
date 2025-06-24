1d20b800b31041051582f1bd36648ce860d60c4c
Urchlove <136825399+Urchlove@users.noreply.github.com>
2025-06-24 15:46:53 +0100

    eig

--- src/component/Pictur/Pict.jsx
+++ src/component/Pictur/Pict.jsx
@@ -8,7 +8,7 @@ function Pict() {
   return (
     <div>
         <h1>HELLO GUYS WELCOME TO MY MINI BIRTHDAY VLOG</h1>
-        <button className='play' onClick={()=> new Audio(mp3).play()}>PLAY</button>
+       <button className='play' onClick={()=> new Audio(mp3).play()}>PLAY</button>
        <div className='imgg' ><img className='img' src={urch} alt='' /> </div> 
     </div>
   )
